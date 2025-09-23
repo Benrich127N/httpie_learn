5e2babe38c8cc05f14bf641955268d00b3ff6a8b
benrich <benrichprince1@gmail.com>
2025-09-23 14:27:53 +0100

    product

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php 
 
+declare(strict_types=1);
+
+spl_autoload_register(function ($class) {
+    require __DIR__ . "/src/$class.php";
+});
+
+
  $parts = explode("/", $_SERVER["REQUEST_URI"]); 
 
- print_r($parts);
+ if ($parts[2] != "products") {
+  http_response_code(404);
+  exit;
+ }
+
+ $id = $parts[3] ?? null;
+
+  $controller = new ProductController;
+  
+
+ $controller->processRequest($_SERVER["REQUEST_METHOD"], $id);
+
+ ?>
